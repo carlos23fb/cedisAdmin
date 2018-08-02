91b90194aea21f0dad9121b2570b42cf02244c5c
jcflobe@gmail.com <jcflobe@gmail.com>
2018-08-02 03:32:57 -0600

    Change navbar

--- clases/header.php
+++ clases/header.php
@@ -34,17 +34,7 @@
 				<li class="nav-item">
 					<a class="nav-link" href="indicadores.php">Indicadores</a>
 				</li>
-				<li class="nav-item dropdown">
-					<a class="nav-link active dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						Dropdown
-					</a>
-					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-						<a class="dropdown-item" href="contact.php">Contact</a>
-						<a class="dropdown-item" href="base_datos.php">Data Base</a>
-						<div class="dropdown-divider"></div>
-						<a class="dropdown-item" href="#">Something else here</a>
-					</div>
-				</li>
+
 
 			</ul>
 
