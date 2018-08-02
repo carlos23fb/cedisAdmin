4be06555a07cc737c4afa5f582984c39b60e72ec
jcflobe@gmail.com <jcflobe@gmail.com>
2018-08-02 04:07:07 -0600

    Añadiendo nuevo CSS

--- DB/users.sql
+++ DB/users.sql
@@ -1,11 +1,6 @@
 
 
-create  table usuarios(
-                      id int autoincrement,
-                      nombre varchar(50),
-                      apellido varchar(50),
-                      usuario varchar(50),
-                      password text(50),
-                      primary key (id)
 
-);
+SELECT sap_empleado, nombre
+ FROM empleados
+WHERE tipo="PREVENTA"
